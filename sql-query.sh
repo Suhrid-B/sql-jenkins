2185be4c76bc2670e883ca0135ed691d426308a3
Suhrid-B <120560093+Suhrid-B@users.noreply.github.com>
2022-12-14 15:28:22 +0530

    Update sql-query.sh

--- sql-query.sh
+++ sql-query.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 sql_user=$1
 sql_pwd=$2
-echo "getting table values for employees"
+echo "getting table values for Employees"
 mysql --user=$sql_user --password=$sql_pwd employees<<EOFMYSQL
 select * from employees order by hire_date;
 EOFMYSQL
